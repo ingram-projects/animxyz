b55b854a846121e72cea1534e44fbcacd6b76f0e
Miles Ingram <milesingrams@gmail.com>
2020-05-08 17:30:47 -0400

    XyzContext directive now allows static xyz attribute

--- packages/vue/src/directives/XyzContext.js
+++ packages/vue/src/directives/XyzContext.js
@@ -1,6 +1,16 @@
 import clsx from 'clsx'
 
-export default function (el, binding) {
-	const xyz = clsx(binding.value)
+function updateDirective(el, binding) {
+	const xyz = clsx(el.staticXyz?.split(' '), binding.value)
 	el.setAttribute('xyz', xyz)
+}
+
+export default {
+	bind(el, binding) {
+		el.staticXyz = el.getAttribute('xyz')
+		updateDirective(el, binding)
+	},
+	update(el, binding) {
+		updateDirective(el, binding)
+	},
 }
