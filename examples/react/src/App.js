7a16388a097a3d4d6a9e683da41ebcb5e7331ecc
Miles Ingram <milesingrams@gmail.com>
2020-05-17 18:04:27 -0400

    Small fix to react examples

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -10,20 +10,20 @@ function App() {
 
 	return (
 		<div>
-			<XyzTransitionGroup xyz="fade down duration-10 stagger">
+			<XyzTransitionGroup appear xyz="fade down duration-10 stagger">
 				{[...Array(5)].map((e, index) => {
 					return <div key={index}>Hello</div>
 				})}
 			</XyzTransitionGroup>
 
-			<XyzTransition state={toggled} mode="out-in" xyz="fade down duration-10">
+			<XyzTransition appear state={toggled} mode="out-in" xyz="fade down duration-10">
 				{!toggled && <button onClick={toggle} xyz="fade flip-left">Toggle</button>}
 				{toggled && <button onClick={toggle}>Untoggle</button>}
 			</XyzTransition>
 
 			{[...Array(100)].map((e, index) => {
 				return (
-					<XyzTransitionVisible xyz="fade down duration-10" key={index}>
+					<XyzTransitionVisible appear xyz="fade down duration-10" key={index}>
 						<div>Visible</div>
 					</XyzTransitionVisible>
 				)
