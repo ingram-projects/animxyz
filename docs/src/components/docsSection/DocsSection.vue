67c884210707325e1a86a5de11f2fd84ac0c03e2
Mattan Ingram <mattaningram@gmail.com>
2020-06-15 01:15:50 -0400

    Better mobile styles

--- docs/src/components/docsSection/DocsSection.vue
+++ docs/src/components/docsSection/DocsSection.vue
@@ -1,20 +1,14 @@
 <template>
 	<section class="docs-section">
-		<div class="section-columns__wrap">
-			<div class="section-column section-text xyz-nested" xyz="inherit fade left" v-if="!mobile || column === 'text'">
-				<div class="section-column__content">
-					<h1 class="section-title">{{ section.title }}</h1>
-					<markdown-content :content="section.content"></markdown-content>
-				</div>
+		<div class="section-column section-text xyz-nested" xyz="inherit fade left">
+			<div class="section-column__content">
+				<h1 class="section-title">{{ section.title }}</h1>
+				<markdown-content :content="section.content"></markdown-content>
 			</div>
-			<div
-				class="section-column section-sandbox xyz-nested"
-				xyz="inherit fade up"
-				v-if="section.examples.length && (!mobile || column === 'sandbox')"
-			>
-				<div class="section-column__content">
-					<sandbox :modifiers="section.modifiers" :examples="section.examples"></sandbox>
-				</div>
+		</div>
+		<div class="section-column section-sandbox xyz-nested" xyz="inherit fade up" v-if="section.examples.length">
+			<div class="section-column__content">
+				<sandbox :modifiers="section.modifiers" :examples="section.examples"></sandbox>
 			</div>
 		</div>
 	</section>
@@ -26,7 +20,7 @@ import Sandbox from '~/components/reusable/Sandbox'
 
 export default {
 	name: 'DocsSection',
-	props: ['section', 'mobile', 'column'],
+	props: ['section', 'mobile'],
 	components: {
 		MarkdownContent,
 		Sandbox,
@@ -38,8 +32,14 @@ export default {
 .docs-section {
 	margin: 0 auto;
 	margin-bottom: 10vw;
+	align-items: flex-start;
+	display: flex;
 
-	@include media('>desktop') {
+	@include media('<tablet') {
+		display: block;
+	}
+
+	@include media('>=desktop') {
 		margin-bottom: 8rem;
 	}
 }
@@ -55,15 +55,14 @@ export default {
 	}
 }
 
-.section-columns__wrap {
-	align-items: flex-start;
-	display: flex;
-}
-
 .section-column {
 	width: 50%;
 
 	&:only-child {
+		width: 100%;
+	}
+
+	@include media('<tablet') {
 		width: 100%;
 	}
 }
@@ -85,5 +84,14 @@ export default {
 	position: sticky;
 	padding: 0 $spacing-m;
 	top: $spacing-m;
+
+	@include media('<tablet') {
+		width: 100vw;
+		padding: 0;
+
+		.sandbox {
+			border-radius: 0;
+		}
+	}
 }
 </style>
