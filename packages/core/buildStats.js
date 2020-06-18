fd76fb48a5755e6ce71ed28f9bdcdf14d2e0f097
Miles Ingram <milesingrams@gmail.com>
2020-06-18 00:37:17 -0400

    Added stats to sass build script

---
+++ packages/core/buildStats.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const gzipSize = require('gzip-size')
+const prettyBytes = require('pretty-bytes')
+
+const files = process.argv.slice(2)
+
+files.forEach((file) => {
+  const stats = fs.statSync(file)
+  const gzipped = gzipSize.fileSync(file)
+
+  console.table({
+    Stats: {
+      File: file,
+      Size: prettyBytes(stats.size),
+      Gzipped: prettyBytes(gzipped),
+    }
+  })
+})
