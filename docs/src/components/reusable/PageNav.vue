0692c95084eb8111d5d3ef6a50989e432fad743e
Miles Ingram <milesingrams@gmail.com>
2020-08-10 22:29:38 -0400

    Fixed css scroll

--- docs/src/components/reusable/PageNav.vue
+++ docs/src/components/reusable/PageNav.vue
@@ -24,8 +24,6 @@
 							class="nav-sections"
 							xyz="fade left"
 							active-class="active"
-							:scroll-on-start="false"
-							:click-to-scroll="false"
 							@itemchanged="onSectionChanged"
 						>
 							<li
