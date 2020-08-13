ca34a10f8290f3ce4f0ea3347cfd1a7220528862
Mattan Ingram <mattaningram@gmail.com>
2020-08-12 22:04:40 -0400

    Fix gradient direction

--- docs/src/components/reusable/AnimXyzLogo.vue
+++ docs/src/components/reusable/AnimXyzLogo.vue
@@ -50,14 +50,13 @@ $logo-size: 2.25rem;
 	@include size(100%);
 	border-radius: $br-l;
 	background-image: linear-gradient(
-		var(--logo-gradient-direction, to left),
+		var(--logo-gradient-direction, to top),
 		rgba(var(--logo-side-color), 1),
 		rgba(var(--logo-side-color), 0.15)
 	);
 }
 
 .side--red {
-	--logo-gradient-direction: to top;
 	--logo-side-color: var(--red-rgb);
 	transform: rotateY(90deg);
 }
@@ -68,6 +67,7 @@ $logo-size: 2.25rem;
 }
 
 .side--green {
+	--logo-gradient-direction: to left;
 	--logo-side-color: var(--green-rgb);
 }
 
