b25890d04062040e76e4a39a40bb1843f1e729b3
Mattan Ingram <mattaningram@gmail.com>
2020-10-09 18:15:50 -0400

    Fix typo

--- docs/src/pages/Index.vue
+++ docs/src/pages/Index.vue
@@ -47,8 +47,8 @@
 							<p>
 								AnimXYZ helps you create, customize, and compose animations for your website with simple HTML attributes
 								and CSS utilities. Instead of writing similar keyframes over and over, or relying on libraries that are
-								limited limited to a specific set, use AnimXYZ to save you time and give you complete control over how
-								your elements move. Built for Vue, React, SCSS, and CSS, AnimXYZ will bring your website to life.
+								limited to a specific set, use AnimXYZ to save you time and give you complete control over how your
+								elements move. Built for Vue, React, SCSS, and CSS, AnimXYZ will bring your website to life.
 							</p>
 						</div>
 					</xyz-transition>
