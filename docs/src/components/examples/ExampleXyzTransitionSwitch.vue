cdc3bf6e372d54216bc7162f09148fa3b43ab70f
Mattan Ingram <mattaningram@gmail.com>
2020-12-13 18:27:20 -0500

    Tweak switch example font size

--- docs/src/components/examples/ExampleXyzTransitionSwitch.vue
+++ docs/src/components/examples/ExampleXyzTransitionSwitch.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="example-wrap flex-col">
 		<XyzTransition appear mode="out-in" xyz="flip-up out-flip-down duration-3 ease-out">
-			<div class="square" :key="customData.key">{{ customData.key }}</div>
+			<div class="square text--xxl fw--bold" :key="customData.key">{{ customData.key }}</div>
 		</XyzTransition>
 		<button class="example-button mt-l" @click="customData.key += 1">Click to switch</button>
 	</div>
