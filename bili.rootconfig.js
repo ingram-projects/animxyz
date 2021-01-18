b5db20bb59efdf2069c9bc152a638b8b0da289b9
Miles Ingram <milesingrams@gmail.com>
2021-01-17 22:34:56 -0500

    fix: tweaked peer dependencies

--- bili.rootconfig.js
+++ bili.rootconfig.js
@@ -21,10 +21,6 @@ module.exports = function (pkg) {
 			},
 			format: ['umd-min', 'esm', 'cjs'],
 		},
-		external: [
-			...Object.keys(pkg.dependencies),
-			...Object.keys(pkg.peerDependencies),
-			...Object.keys(pkg.devDependencies),
-		],
+		external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.devDependencies)],
 	}
 }
