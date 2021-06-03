4e3cd2b509232f36cfbe6102ece600dbdf3953d6
Miles Ingram <milesingrams@gmail.com>
2021-06-03 15:12:06 -0700

    refactor: added AppearVisible example

---
+++ examples/react/src/examples/XyzTransitionGroup/AppearVisible.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { XyzTransitionGroup } from '@animxyz/react'
+import './AppearVisible.scoped.scss'
+
+export default function Example() {
+	return (
+		<div className="example-wrap">
+			<div className="scroll-wrap">
+				<XyzTransitionGroup appearVisible class="example-grid" xyz="delay-2 fade small rotate-right">
+					{[...Array(200)].map((_, index) => (
+						<div className="square" key={index} />
+					))}
+				</XyzTransitionGroup>
+			</div>
+		</div>
+	)
+}
---
+++ examples/vue3/src/examples/XyzTransitionGroup/AppearVisible.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="example-wrap">
+		<div class="scroll-wrap">
+			<XyzTransitionGroup appear-visible class="example-grid" xyz="delay-2 fade small rotate-right">
+				<div class="square" v-for="index in 200" :key="index"></div>
+			</XyzTransitionGroup>
+		</div>
+	</div>
+</template>
+
+<script>
+import ExampleMixin from '../ExampleMixin'
+
+export default {
+	mixins: [ExampleMixin],
+}
+</script>
+
+<style lang="scss" scoped>
+.scroll-wrap {
+	overflow-y: auto;
+	max-height: 28rem;
+}
+
+.example-grid {
+	gap: $sp-xxs;
+}
+</style>
